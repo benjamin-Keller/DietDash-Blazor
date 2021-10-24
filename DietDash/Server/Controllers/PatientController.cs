ea6483265bbd3d664fee1b7caba22247c1a898c4
benjamin.Keller <zhero240@gmail.com>
2021-10-25 00:15:50 +0200

    Adds PatientController on Server

---
+++ DietDash/Server/Controllers/PatientController.cs
@@ -0,0 +1,76 @@
+﻿using DietDash.Shared;
+using DietDash.Shared.Patient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DietDash.Server.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PatientController : ControllerBase
+    {
+        static List<Dietitian> dietitans = new List<Dietitian>
+        {
+            new Dietitian
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Inge",
+                LastName = "Jung",
+                IdNumber = "987456321456",
+                EmailAddress = "Test@Test.com"
+            }
+        };
+
+        List<Patient> patients = new List<Patient>
+        {
+            new Patient
+            {
+                DietitianId = dietitans[0].Id,
+                FileName = "TestFileName",
+                FirstName = "TestFirst",
+                SecondName = "TestSecond",
+                LastName = "TestLast",
+                DateOfBirth = DateTime.Now.AddYears(-21),
+                CreatedOn = DateTime.Now,
+                CreatedBy = "TestDietitian",
+                IdNumber = "9844569875548",
+                Anthropology = anthropologies[0]
+            },
+            new Patient
+            {
+                DietitianId = dietitans[0].Id,
+                FileName = "TestFileNameSecond",
+                FirstName = "TestFirstSecond",
+                LastName = "TestLastSecond",
+                DateOfBirth = DateTime.Now.AddYears(-18),
+                CreatedOn = DateTime.Now,
+                CreatedBy = "TestDietitian",
+                IdNumber = "9844569875548",
+                Anthropology = anthropologies[1]
+            }
+        };
+
+        static List<Anthropology> anthropologies = new List<Anthropology>
+        {
+            new Anthropology
+            {
+                Height = 185,
+                Weight = 66,
+            },
+            new Anthropology
+            {
+                Height = 168,
+                Weight = 56,
+            }
+        };
+
+        [HttpGet(Name = "GetPatients")]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(patients);
+        }
+    }
+}
