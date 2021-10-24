e2f9cb6e3b7d61440e2e875bf9310f492485cd3f
benjamin.Keller <zhero240@gmail.com>
2021-10-25 00:14:43 +0200

    Adds POCOs

---
+++ DietDash/Shared/Patient/Anthropology.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DietDash.Shared.Patient
+{
+    public class Anthropology
+    {
+        public Guid PatientId { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+    }
+}
---
+++ DietDash/Shared/Patient/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DietDash.Shared.Patient
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public Guid DietitianId { get; set; }
+        public string FileName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string IdNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+
+        public Anthropology Anthropology { get; set; }
+    }
+}
